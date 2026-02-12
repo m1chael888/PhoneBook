6e962c52a54e772589612f7dc91bd6cc010a2794
m1chael888 <sofishmain@gmail.com>
2026-02-12 23:52:51 +0200

    basic setup

---
+++ PhoneBook.m1chael888/Infrastructure/Router.cs
@@ -0,0 +1,14 @@
+﻿namespace PhoneBook.m1chael888.Infrastructure
+{
+    public interface IRouter
+    {
+        void Route();
+    }
+    public class Router : IRouter
+    {
+        public void Route()
+        {
+
+        }
+    }
+}
--- PhoneBook.m1chael888/Program.cs
+++ PhoneBook.m1chael888/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace PhoneBook.m1chael888
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneBook.m1chael888.Infrastructure;
+
+namespace PhoneBook.m1chael888
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var collection = new ServiceCollection();
+
+            collection.AddScoped<IRouter, Router>();
+
+            var provider = collection.BuildServiceProvider();
+
+            //resolve controller(s) to pass to router
+
+            var router = provider.GetRequiredService<IRouter>();
+            router.Route();
         }
     }
 }
