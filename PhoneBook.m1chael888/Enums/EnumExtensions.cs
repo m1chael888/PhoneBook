2777ca0578ea0054bc710e2ac0c0cc97734cbaee
m1chael888 <sofishmain@gmail.com>
2026-02-20 23:35:43 +0200

    enum extension method

---
+++ PhoneBook.m1chael888/Enums/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace PhoneBook.m1chael888.Enums
+{
+    internal class EnumExtensions
+    {
+        public static string GetDescription(Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attributes = (DescriptionAttribute[])field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+            return value.ToString();
+        }
+    }
+}
